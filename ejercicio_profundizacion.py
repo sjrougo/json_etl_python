a38da4d65d39175ec347dcbcf74205db907c3e71
sjrougo <107430928+sjrougo@users.noreply.github.com>
2022-09-08 09:49:39 -0300

    Add files via upload

--- ejercicio_profundizacion.py
+++ ejercicio_profundizacion.py
@@ -13,15 +13,10 @@ def fetch():
     data = response.json()
     for dato in data['results']:
         resultados.append(dato)
-        #print(dato, '\n')
-    #resultados = response['results']
-    #dataset = [resultados[x] for x in range(0, len(resultados)) if resultados[x]["currency_id"] == 'ARS' ]
-    #{"price": 1250, "condition": "used"}
     for x in range(0, len(resultados)):
         if resultados[x]["currency_id"] == 'ARS':
             dicc = {'price': resultados[x]['price'], 'condition': resultados[x]['condition']}
             dataset.append(dicc)
-    #print(dataset)
     return dataset
 
 def transform(dataset, min, max):
@@ -36,7 +31,8 @@ def transform(dataset, min, max):
     min_count = len(lista_min)
     min_max_count = len(lista_med)
     max_count = len(lista_max)
-    
+    # No encontré la forma de realizar comprensión de listas, así que se puede observar una repetición de
+    # códigos en esta función
     return [min_count, min_max_count, max_count]
 
 def report (lista):
@@ -58,8 +54,8 @@ def report (lista):
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-
+    # En el ejercicio escribo dos valores de compromiso para min y max, al solo efecto de realizar el ejercicio
     fetch()
-    print(transform(fetch(), 20000, 200000))
+    #print(transform(fetch(), 20000, 200000))
     report(transform(fetch(), 20000, 200000))
     print("terminamos")
